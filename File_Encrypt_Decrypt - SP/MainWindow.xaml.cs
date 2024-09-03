948204f08537743219f301a8b6f60561ba0e4550
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-09-03 16:13:46 +0400

    :

--- File_Encrypt_Decrypt - SP/MainWindow.xaml.cs
+++ File_Encrypt_Decrypt - SP/MainWindow.xaml.cs
@@ -1,24 +1,148 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
-namespace File_Encrypt_Decrypt___SP
+namespace File_Encrypt_Decrypt___SP;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private CancellationTokenSource _cts;
+    public string Parol = "44";
+    public bool CheckStart = true;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+        if (openFileDialog.ShowDialog() == true)
         {
-            InitializeComponent();
+            FilePathTextBox.Text = openFileDialog.FileName;
         }
+    }
+
+    private void StartButton_Click(object sender, RoutedEventArgs e)
+    {
+        string filePath = FilePathTextBox.Text;
+        string password = PasswordBox.Password;
+
+        if (string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Please Selected File or Password !");
+            return;
+        }
+
+        _cts = new CancellationTokenSource();
+        CancellationToken token = _cts.Token;
+
+        if(password != Parol)
+        {
+            MessageBox.Show($"Password -> {Parol}");
+            return;
+        }
+
+        if (EncryptRadioButton.IsChecked == true)
+        {
+            ThreadPool.QueueUserWorkItem(_ => EncryptFile(filePath, password, token));
+        }
+        else if (DecryptRadioButton.IsChecked == true)
+        {
+            ThreadPool.QueueUserWorkItem(_ => DecryptFile(filePath, password, token));
+        }
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        CheckStart = false;
+        for (int i = 0; i < ProgressBar.Value; i++)
+        {
+            if (ProgressBar.Value == 0) return;
+            Dispatcher.Invoke(() => 
+            {
+                ProgressBar.Value --;
+                Thread.Sleep(10);
+                btnStart.IsEnabled = false; 
+                btnCancel.IsEnabled = false;
+            });
+        }
+
+        Dispatcher.Invoke(() =>
+        {
+            ProgressBar.Value = 0;
+            btnStart.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+        });
+        _cts?.Cancel();
+        MessageBox.Show("Canel");
+    }
+
+    private void EncryptFile(string filePath, string password, CancellationToken token)
+    {
+        try
+        {
+            byte[] fileBytes = File.ReadAllBytes(filePath);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            Dispatcher.Invoke(() =>
+            {
+                ProgressBar.Value = 0;
+                ProgressBar.Maximum = fileBytes.Length;
+                btnStart.IsEnabled = false;
+            });
+
+            for (int i = 0; i < fileBytes.Length; i++)
+            {
+                if (CheckStart)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        if (CheckStart)
+                        {
+                            ProgressBar.Value++;
+                            Thread.Sleep(10);
+                            _cts.Cancel();
+                        }
+                        else
+                        {
+                            ProgressBar.Value = 0;
+                            FilePathTextBox.Text = null;
+                            PasswordBox.Password = null;
+                            btnStart.IsEnabled = true;
+                            btnCancel.IsEnabled = true;
+                            return;
+                        }
+                    });
+                    fileBytes[i] = (byte)(fileBytes[i] ^ passwordBytes[i % passwordBytes.Length]);
+                }
+                else return;
+            }
+
+
+            Dispatcher.Invoke(() => {
+
+                ProgressBar.Value = 0;
+                FilePathTextBox.Text = null;
+                PasswordBox.Password = null;
+                btnStart.IsEnabled = true;
+            });
+
+            File.WriteAllBytes(filePath, fileBytes);
+            MessageBox.Show("Success");
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void DecryptFile(string filePath, string password, CancellationToken token)
+    {
+        EncryptFile(filePath, password, token);
     }
 }
