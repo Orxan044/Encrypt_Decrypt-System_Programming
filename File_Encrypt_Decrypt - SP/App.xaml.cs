4769973c136fc3e0ea6d7b9369e66a35ea0760be
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-09-03 10:47:46 +0400

    Add project files.

---
+++ File_Encrypt_Decrypt - SP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace File_Encrypt_Decrypt___SP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
